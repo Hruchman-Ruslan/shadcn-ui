d0e9c45656461d16b0e9e74e7201bc95f0ff8343
Ruslan <Giovingo92@gmail.com>
2025-03-27 17:25:49 +0200

    finish project

--- app/dashboard/employees/columns.tsx
+++ app/dashboard/employees/columns.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import { Badge } from '@/components/ui/badge'
 import { ColumnDef } from '@tanstack/react-table'
+import Image from 'next/image'
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -17,6 +20,28 @@ export const columns: ColumnDef<Employee>[] = [
 	{
 		accessorKey: 'avatar',
 		header: '',
+		cell({ row }) {
+			const avatar: string = row.getValue('avatar')
+			const firstName: string = row.getValue('firstName')
+			const lastName: string = row.getValue('lastName')
+
+			return (
+				<Avatar>
+					{!!avatar && (
+						<Image
+							width={40}
+							height={40}
+							src={avatar}
+							alt={`${firstName} ${lastName} avatar`}
+						/>
+					)}
+					<AvatarFallback className='uppercase'>
+						{firstName[0]}
+						{lastName[0]}
+					</AvatarFallback>
+				</Avatar>
+			)
+		},
 	},
 	{
 		accessorKey: 'firstName',
@@ -33,5 +58,11 @@ export const columns: ColumnDef<Employee>[] = [
 	{
 		accessorKey: 'isTeamLeader',
 		header: '',
+		cell({ row }) {
+			const isTeamLeader: boolean = row.getValue('isTeamLeader')
+			return isTeamLeader ? (
+				<Badge variant={'success'}>Team Leader</Badge>
+			) : null
+		},
 	},
 ]
